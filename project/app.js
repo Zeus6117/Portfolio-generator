e2ab27299e2eb1d248b8c4d2bfa5671a35eb28bc
Jordan King <96736562+Zeus6117@users.noreply.github.com>
2022-04-14 10:04:34 -0400

    created .gitignore file

--- project/app.js
+++ project/app.js
@@ -1,17 +1,31 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
+//const fs = require('fs');
+
+//const generatePage = require('./src/page-template.js');
+
+//const pageHTML = generatePage(name, github)
+/*we're using inquirer to capture date so we no longer need this to capture the date
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-/*const name = profileDataArgs[0];
-const github = profileDataArgs[1]; (assignment destructing below)*/
-const [name, github] = profileDataArgs;
+const name = profileDataArgs[0];
+const github = profileDataArgs[1]; (assignment destructing below)
+const [name, github] = profileDataArgs;*/
 
 
 
-  fs.writeFile('index.html', generatePage(name, github), err => {
-      if (err) throw err;
+ // fs.writeFile('./index.html', pageHTML, err => {
+    //  if (err) throw err;
 
-      console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+   //   console.log('Portfolio complete! Check out index.html to see the output!');
+ // });
